24ebe7134415977ce839461135cd9df08262900f
Ahmed Reza Rafsanzani <rza272@gmail.com>
2022-12-12 13:44:56 +0700

    feat: support configurable cookie domain

--- cookie.go
+++ cookie.go
@@ -6,11 +6,12 @@ package osin
 
 import (
 	"net/http"
+	"net/url"
 	"time"
 )
 
 // AddTokenInCookie adds token cookie in the response header
-func AddTokenInCookie(response *Response, token string, tokenType string, tokenExpiration int64) {
+func AddTokenInCookie(response *Response, token string, tokenType string, tokenExpiration int64, cookieDomain string) {
 	cookie := http.Cookie{
 		Name:     tokenType,
 		Value:    token,
@@ -20,6 +21,14 @@ func AddTokenInCookie(response *Response, token string, tokenType string, tokenE
 		SameSite: http.SameSiteNoneMode,
 		Path:     "/",
 	}
+
+	if cookieDomain != "" {
+		parsed, err := url.Parse(cookieDomain)
+		if err == nil {
+			cookie.Domain = parsed.Host
+		}
+	}
+
 	if v := cookie.String(); v != "" {
 		response.Headers.Add("Set-Cookie", v)
 	}
